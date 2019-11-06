d733684c4d3e37a80102401814fad10cc6561d1e
chenyinzhe <13005684532@163.com>
2019-11-06 14:07:44 +0800

    6-1 可以更改购物车内购买的商品数量

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,16 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    if !current_cart.products.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "成功将#{@product.title}加入购物车"
+    else
+      flash[:notice] = "你的购物车中已有此产品"
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,19 @@ Rails.application.routes.draw do
     resources :products
   end
 
-  resources :products
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
+
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
+
+  resources :cart_items
 
   root 'products#index'
 end
